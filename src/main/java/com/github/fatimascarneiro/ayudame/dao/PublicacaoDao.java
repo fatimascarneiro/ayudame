63baca04b3e2af6467be00c50ab52f93a1be5156
fatimacarneiro <fatima.carneiro@bluesoft.com.br>
2020-04-22 16:29:31 -0300

    #2 - Tentando corrigir para fazer subirrrrrr

--- src/main/java/com/github/fatimascarneiro/ayudame/dao/PublicacaoDao.java
+++ src/main/java/com/github/fatimascarneiro/ayudame/dao/PublicacaoDao.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+//@Repository
 public class PublicacaoDao {
 
 //TODO FICARÁ ASSIM ATÉ EU IMPLEMENTAR O BANCO DE DADOS
--- src/main/java/com/github/fatimascarneiro/ayudame/services/CategoriaDeServicosService.java
+++ src/main/java/com/github/fatimascarneiro/ayudame/services/CategoriaDeServicosService.java
@@ -1,55 +1,55 @@
-package com.github.fatimascarneiro.ayudame.services;
-
-import com.github.fatimascarneiro.ayudame.vitrine.categoriadeservico.CategoriaDeServicos;
-import com.github.fatimascarneiro.ayudame.dao.CategoriaDeServicosDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class CategoriaDeServicosService {
-
-    private String nome;
-
-    private String descricao;
-
-    private final CategoriaDeServicosDao dao;
-
-    @Autowired
-    public CategoriaDeServicosService(CategoriaDeServicosDao dao) {
-        this.dao = dao;
-    }
-
-    public String getNome() {
-        return (nome != null) ? nome : "";
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return (descricao != null) ? descricao : "";
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public CategoriaDeServicos cadastraCategoriaDeServico(String nome, String descricao) {
-        return  dao.cadastraCategoriaDeServico(nome, descricao);
-    }
-
-    public void deletarCategoriaDeServico(int id) {
-        dao.deletarCategoriaDeServico(id);
-    }
-
-    public List<CategoriaDeServicos> listarCategoriaDeServicoPorNome(String nome) {
-        return dao.listarCategoriaDeServicoPorNome(nome);
-    }
-
-    public CategoriaDeServicos editarCategoriaDeServico(int categoriaDeServicoKey, String nome, String descricao) {
-        return dao.editarCategoriaDeServico(categoriaDeServicoKey, nome, descricao);
-    }
-}
+//package com.github.fatimascarneiro.ayudame.services;
+//
+//import com.github.fatimascarneiro.ayudame.vitrine.categoriadeservico.CategoriaDeServicos;
+//import com.github.fatimascarneiro.ayudame.dao.CategoriaDeServicosDao;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class CategoriaDeServicosService {
+//
+//    private String nome;
+//
+//    private String descricao;
+//
+//    private final CategoriaDeServicosDao dao;
+//
+//    @Autowired
+//    public CategoriaDeServicosService(CategoriaDeServicosDao dao) {
+//        this.dao = dao;
+//    }
+//
+//    public String getNome() {
+//        return (nome != null) ? nome : "";
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getDescricao() {
+//        return (descricao != null) ? descricao : "";
+//    }
+//
+//    public void setDescricao(String descricao) {
+//        this.descricao = descricao;
+//    }
+//
+//    public CategoriaDeServicos cadastraCategoriaDeServico(String nome, String descricao) {
+//        return  dao.cadastraCategoriaDeServico(nome, descricao);
+//    }
+//
+//    public void deletarCategoriaDeServico(int id) {
+//        dao.deletarCategoriaDeServico(id);
+//    }
+//
+//    public List<CategoriaDeServicos> listarCategoriaDeServicoPorNome(String nome) {
+//        return dao.listarCategoriaDeServicoPorNome(nome);
+//    }
+//
+//    public CategoriaDeServicos editarCategoriaDeServico(int categoriaDeServicoKey, String nome, String descricao) {
+//        return dao.editarCategoriaDeServico(categoriaDeServicoKey, nome, descricao);
+//    }
+//}
